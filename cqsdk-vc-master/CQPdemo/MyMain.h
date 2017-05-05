5f460b9c295d93af80c3038bb18211196cd76380
ICEAGE-PC\ICE AGE <465578756@qq.com>
2017-05-05 11:54:11 +0800

    代码

---
+++ cqsdk-vc-master/CQPdemo/MyMain.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "cqp.h"
+#include "stdafx.h"
+class MyMain
+{
+public:
+	~MyMain();
+	static MyMain * getInstance();
+	static void destroyInstance();
+	void setAcCode(int ac);
+	//收到私聊消息
+	void onRecPrivateMsg(int32_t subType, int32_t sendTime, int64_t fromQQ, const char *msg, int32_t font);
+	//收到群聊消息
+	void onRecGroupMsg(int32_t subType, int32_t sendTime, int64_t fromGroup, int64_t fromQQ, const char *fromAnonymous, const char *msg, int32_t font);
+private:
+	//发送私聊消息
+	void sendPrivateMsg(const char *msg, int64_t qqid); 
+	//发送群聊消息
+	void sendGroupMsg(const char *msg, int64_t qqid);
+	int acCode = 0;
+	void loadMenuConfig();
+	void xLog(const char *msg);
+	void xLog(int64_t qqid, const char *msg);
+	FILE * logF=nullptr;
+	char logBuf[10 * 1024];
+};
